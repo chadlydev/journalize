d71ab25ad24e6cae0b7268659a0a1f3cd72d7e64
Chadly Riedewald <no925.c@gmail.com>
2023-04-15 15:42:53 +0200

    added auth context

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { JournalContextProvider, AuthContextProvider } from 'context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <JournalContextProvider>
+        <App />
+      </JournalContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
 
